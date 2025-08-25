e6dde8a01274b4a49d6fad80d61f5fa6c4aa20fa
magser110 <124111918+magser110@users.noreply.github.com>
2025-08-25 14:56:44 -0500

    saving progress on setting up front end and user, auth

---
+++ running_app/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthenticationService, private router: Router){}
+
+  login(){
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res: any) => {
+        console.log('Logged in with token:', res.token);
+        this.authService.setToken(res.token);
+        // this.router.navigate(['/run'])
+
+      },
+      error: (error: any) => {
+        console.error('Loggin error', error);
+      }
+    })
+  }
+
+}
